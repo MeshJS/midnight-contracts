eaa01854a1121af5523418afa06295e2a00d1b0d
erick <erickrs2esuarez@gmail.com>
2025-08-19 17:45:26 -0500

    bboard contract

--- bulletin-board/src/index.ts
+++ bulletin-board/src/index.ts
@@ -1,2 +1,2 @@
-export * as Counter from "./managed/counter/contract/index.cjs";
+export * as Counter from "./managed/board/contract/index.cjs";
 export * from "./witnesses";
--- bulletin-board/src/witnesses.ts
+++ bulletin-board/src/witnesses.ts
@@ -1,12 +1,19 @@
-// This is how we type an empty object.
-export type CounterPrivateState = {
-  privateCounter: number;
+import { Ledger } from "./managed/board/contract/index.cjs";
+import { WitnessContext } from "@midnight-ntwrk/compact-runtime";
+
+export type BBoardPrivateState = {
+  readonly secretKey: Uint8Array;
 };
 
-export const createPrivateState = (value: number): CounterPrivateState => {
-  return {
-    privateCounter: value,
-  };
-};
+export const createBBoardPrivateState = (secretKey: Uint8Array) => ({
+  secretKey,
+});
 
-export const witnesses = {};
+export const witnesses = {
+  localSecretKey: ({
+    privateState,
+  }: WitnessContext<Ledger, BBoardPrivateState>): [
+    BBoardPrivateState,
+    Uint8Array,
+  ] => [privateState, privateState.secretKey],
+};
